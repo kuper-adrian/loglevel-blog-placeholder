06e91e2a2faf8ef6f30d785f53e2b5ad309bf5fa
Adrian Kuper <adrian.kuper@hotmail.de>
2018-07-06 18:02:02 +0200

    added about and legal pages and updated style a bit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'loglevel: blog' });
-});
+router
+  .get('/', function(req, res) {
+    res.render('index', { title: 'log-level: blog' });
+  })
+  .get('/about', (req, res) => {
+    res.render('about', { title: 'about - log-level: blog' });
+  })
+  .get('/impressum', (req, res) => {
+    res.render('impressum', { title: 'impressum - log-level: blog' });
+  });
 
 module.exports = router;
